546ccb21669aae6a115f490bcdceacbb126e9a5b
Void1315 <381848900@qq.com>
2017-08-08 17:30:05 +0800

    .

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Support\Facades\Mail;
+use App\Mail\OrderShipped;
 
 class RegisterController extends Controller
 {
