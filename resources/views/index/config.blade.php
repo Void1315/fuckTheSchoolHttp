db4f1baf9ce3d874743deb65b3a3864dc881ccf5
Void1315 <381848900@qq.com>
2017-08-09 08:23:27 +0800

    .

--- resources/views/index/config.blade.php
+++ resources/views/index/config.blade.php
@@ -172,7 +172,7 @@ $('.lnr-pencil').on('click',function()
 		$(obj).css('display','none');
 		$(obj).prev().css('display','');
 		$(obj).prev().prev().css('display','block');
-		if($(obj).attr("id")!='stu_passwd');
+		if($(obj).attr("id")!='stu_passwd')
 			post_ajax(url="{{url('/config')}}",obj);
 		else
 			post_ajax(url="{{url('/config/stupasswd')}}",obj,"stu-from");
