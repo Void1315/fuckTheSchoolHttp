1e03d64385ae5a72c1e40b2d4ef8036133ce3057
Void1315 <381848900@qq.com>
2017-09-01 18:22:14 +0800

    改了一点小问题

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -272,8 +272,10 @@
 						setTime()
 						changeTable($('#a_l_sel').children()[0],'2016-1')
 						if(data.type=='error')
+						{
 							layer.msg('学号密码密码错误',{icon:5})
 							return
+						}
 						else
 							location.reload(true)
 					},
